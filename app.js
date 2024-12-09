5b0569d7ff13a1d5473f9d59a9959cfc60104ea4
Neeraj Sharma <neeraj667589a@gmail.com>
2024-12-09 20:30:02 +0530

    User schema

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 require('dotenv').config();
 
 main()
@@ -38,6 +41,10 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize);
+app.use(passport.session);
+passport.use(new LocalStrategy(User.authenticate()));
 
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
